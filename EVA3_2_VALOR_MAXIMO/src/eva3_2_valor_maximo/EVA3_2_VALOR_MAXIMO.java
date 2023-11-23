0f8c88a2a343830f48e5de4eadba95fed790b298
NestorAec26 <143562624+NestorAec26@users.noreply.github.com>
2023-11-23 17:52:49 -0600

    Add files via upload

---
+++ EVA3_2_VALOR_MAXIMO/src/eva3_2_valor_maximo/EVA3_2_VALOR_MAXIMO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_2_valor_maximo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_2_VALOR_MAXIMO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //llamada a funcion(invocar funcion)
+        //utilizar el valor directamente de la funcion
+        System.out.println(buscarMaximo(100, 200));
+        // guardar el valor y usarlo despues
+        int resu = buscarMaximo(100, 200);
+        System.out.println(resu);
+        //llamar a la funcion e ignorar el resultado
+        buscarMaximo(100, 200);
+    }
+    
+    public static int buscarMaximo(int val1, int val2){
+        /*if(val1 > val2)
+            return val1;
+        else
+            return val2;*/
+        
+        int resultado;
+        if(val1 > val2)
+            resultado = val1;
+        else
+            resultado = val2;
+        
+        return resultado;
+        
+    }
+    
+}
